7367376ef527610e4aa729fc4a2738f3babf3d8f
unknown <muhammad.fajarjati@gmail.com>
2024-03-28 01:06:53 +0700

    first

---
+++ resources/views/started/home.blade.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Righteous&display=swap"
+        rel="stylesheet">
+
+    <title>CleanClass</title>
+    <link rel="icon" href="assets/images/icon.ico">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-gqqflRCy6cJhpzLAc45h5zkNzdpc+ra04+GmyU8dVtNVYuhO4fukn7D/5lxMC8OV" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/template-started.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+        <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#top" class="logo">
+                            <img src="assets/images/logo.png" alt="" width="40" height="40">
+
+                        </a>
+                        <!-- ***** Logo End ***** -->
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#how-to-work">How to Work</a></li>
+                            <li class="scroll-to-section"><a href="#about">About</a></li>
+                            <li class="scroll-to-section">
+                                <div class="border-first-button"><a href="#contact">Login</a></div>
+                            </li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Area Start ***** -->
+    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div class="container">
+            <div class="row">
+                <div class="footer col-lg-12 ">
+                    <div class="row">
+                        <div class="col-lg-6 align-self-center">
+                            <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                                data-wow-delay="1s">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h2>Selamat Datang di CleanClass</h2>
+                                        <p>Sebuah platform website inovatif untuk meningkatkan kualitas kebersihan kelas
+                                            dan mewujudkan lingkungan belajar yang bersih dan sehat!</p>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="border-first-button scroll-to-section">
+                                            <a href="#contact">Login</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                                <img src="assets/images/started.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="mb-1" />
+    <!-- ***** Main Area End ***** -->
+
+    <!-- ***** How to work Area Start ***** -->
+    <div id="how-to-work" class="services section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-heading wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+                        <h4>How <em>CleanClass</em> Work?</h4>
+                        <div class="line-dec"></div>
+                        <br>
+                        <p>Website ini bekerja dengan melakukan beberapa hal penting yang menarik, diantaranya:</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                        data-wow-delay="0.5s">
+                        <i class="fas"> <img src="assets/images/Signing.png" alt=""></i>
+                        <h4>Proses Masuk</h4>
+                        <p>Siswa dan wali kelas dapat masuk ke dalam platform dengan akun mereka masing-masing.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInRight" data-wow-duration="1s"
+                        data-wow-delay="0.5s">
+                        <i class="fas"> <img src="assets/images/Agenda.png" alt=""></i>
+                        <h4>Penjadwalan Piket</h4>
+                        <p>Siswa dapat melihat jadwal piket mereka di dashboard mereka setelah masuk.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                        data-wow-delay="0.5s">
+                        <i class="fas"> <img src="assets/images/Approval.png" alt=""></i>
+                        <h4>Persetujuan</h4>
+                        <p>Wali kelas memeriksa dan menyetujui kehadiran siswa dalam piket tiap minggunya.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInRight" data-wow-duration="1s"
+                        data-wow-delay="0.5s">
+                        <i class="fas"> <img src="assets/images/popular.png" alt=""></i>
+                        <h4>Perhitungan Point</h4>
+                        <p>Setiap kali siswa hadir pada piket, mereka mendapatkan poin dari wali kelasnya dan point akan
+                            dijumlahkan untuk tiap kelas.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                        data-wow-delay="1s">
+                        <i class="fas"> <img src="assets/images/Leaderboard.png" alt=""></i>
+                        <h4>Pengelolaan Leaderboard</h4>
+                        <p>Leaderboard menampilkan kelas mana yang memiliki total poin tertinggi.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-item show-up header-text wow fadeInRight" data-wow-duration="1s"
+                        data-wow-delay="1s">
+                        <i class="fas"> <img src="assets/images/Report.png" alt=""></i>
+                        <h4>Pemantauan dan Pelaporan</h4>
+                        <p>Laporan dapat dibuat untuk menganalisis partisipasi siswa dan peringkat kelas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ***** How to work Area End ***** -->
+
+    <!-- ***** About Area Start ***** -->
+    <footer id="about" class="text-center text-lg-start text-white" style="background-color: #50b0e0">
+        <!-- Grid container -->
+        <div class="p-4 pb-0">
+            <!-- Section: Links -->
+            <section>
+                <!--Grid row-->
+                <div class="row wow fadeInUp">
+                    <!--Grid column 1-->
+                    <div class="col-lg-4 mb-4 mb-lg-0">
+                        <div class="right-image wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">
+                            <img src="assets/images/cleanclasslogo.png" alt="">
+                        </div>
+                    </div>
+                    <!--Grid column 2-->
+                    <div class="col-lg-4 mb-4 mb-lg-0">
+                        <div class="created-by">
+                            <h5 class="text-black fw-bold">Created by</h5>
+                            <ul>
+                                <li>
+                                    <a class="text-white">Laravel</a>
+                                </li>
+                            </ul>
+                            <h5 class="powered text-black fw-bold">Powered by</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a class="text-white">wilimaxs.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--Grid column 3-->
+                    <div class="col-lg-4">
+                        <h5 class="text-black fw-bold">Section</h5>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#" class="text-white">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-white">How to Work</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-white">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <br>
+        <!-- Grid container -->
+
+        <!-- Additional row -->
+        <div class="container">
+            <div class="row wow fadeInUp">
+                <div class="col-lg-6 mx-auto">
+                    <h5 class="text-black fw-bold">Be our friends</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-white">Firda Rosela Sundari</a>
+                        </li>
+                        <li>
+                            <a class="text-white">Hammam Abdurrahman</a>
+                        </li>
+                        <li>
+                            <a class="text-white">Muhammad Fajar Jati Permana</a>
+                        </li>
+                        <li>
+                            <a class="text-white">Najwa Ikhsaniyah</a>
+                        </li>
+                        <li>
+                            <a class="text-white">Wildan Tisna Nugraha</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 mx-auto">
+                    <h5 class="text-black fw-bold">Universitas Pendidikan Indonesia</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-white">Jl. Pendidikan No.15, Cibiru Wetan, Kec. Cileunyi, Kabupaten Bandung,
+                                Jawa Barat 40625</a>
+                        </li>
+                        <li>
+                            <a href="https://kd-cibiru.upi.edu/" class="text-white"><i
+                                    class="fas fa-globe  text-black"></i>
+                                https://kd-cibiru.upi.edu/</a>
+                        </li>
+                        <li>
+                            <a class="text-white"><i class="fas fa-phone  text-black"></i> (022) 7801840</a>
+                        </li>
+                        <li>
+                            <a href="mailto:kampus_cibiru@upi.edu" class="text-white"><i
+                                    class="fas fa-envelope  text-black"></i>
+                                kampus_upicibiru@upi.edu</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!-- End additional row -->
+
+        <!-- Copyright -->
+        <div class="bawah text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            Copyright by Kelompok 2 - Proyek Konsultasi 2024
+        </div>
+    </footer>
+    <!-- ***** About Area End ***** -->
+
+    <!-- Scripts -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/animation.js"></script>
+    <script src="assets/js/imagesloaded.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
