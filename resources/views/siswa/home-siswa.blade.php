b8a819c286ca857d951f392fd88c39d83030f002
unknown <muhammad.fajarjati@gmail.com>
2024-04-18 16:17:09 +0700

    add home siswa

--- resources/views/siswa/home-siswa.blade.php
+++ resources/views/siswa/home-siswa.blade.php
@@ -1,1 +1,144 @@
 @extends('siswa/.layout')
+
+@section('content')
+    <!-- MAIN -->
+    <main>
+        {{-- <div class="head-title">
+            <div class="left">
+                <h1>Over<em>view</em></h1>
+                </ul>
+            </div>
+        </div> --}}
+
+        <ul class="box-info">
+            <li>
+                <i class='bx bxs-user'></i>
+                <span class="text">
+                    <h3>Wali Kelas</h3>
+                    <p>Wildan Tisna Surya M.T.</p>
+                </span>
+            </li>
+            {{-- <li>
+                <i class='bx bxs-door-open'></i>
+                <span class="text">
+                    <h3>Kelas</h3>
+                    <p>7B</p>
+                </span>
+            </li> --}}
+            <li>
+                <i class='bx bxs-medal'></i>
+                <span class="text">
+                    <h3>Point</h3>
+                    <p>20</p>
+                </span>
+            </li>
+            <li>
+                <i class='bx bxs-calendar'></i>
+                <span class="text">
+                    <h3>Berikutnya</h3>
+                    <p>Senin, 22 April 2024</p>
+                </span>
+            </li>
+        </ul>
+
+        <div class="table-data">
+            <div class="order">
+                <div class="head">
+                    <h3>Laporan</h3>
+                    <a href="#" class="btn-download">
+                        <span class="text">See all</span>
+                        <i class='bx bx-right-arrow-alt'></i>
+                    </a>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Waktu</th>
+                            <th>Status</th>
+                            <th>Keterangan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>Senin, 4 Maret 2024</p>
+                            </td>
+                            <td>
+                                <i class='bx bxs-check-circle'></i>
+                            </td>
+                            <td><span class="status completed">Piket</span></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Senin, 26 Februari 2024</p>
+                            </td>
+                            <td>
+                                <i class='bx bxs-x-circle'></i>
+                            </td>
+                            <td><span class="status pending">Tidak Piket</span></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Senin, 19 Februari 2024</p>
+                            </td>
+                            <td>
+                                <i class='bx bxs-check-circle'></i>
+                            </td>
+                            <td><span class="status completed">Piket</span></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Senin, 12 Februari 2024</p>
+                            </td>
+                            <td>
+                                <i class='bx bxs-check-circle'></i>
+                            </td>
+                            <td><span class="status completed">Piket</span></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Senin, 5 Februari 2024</p>
+                            </td>
+                            <td>
+                                <i class='bx bxs-check-circle'></i>
+                            </td>
+                            <td><span class="status completed">Piket</span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="todo">
+                <div class="head">
+                    <h3>Leaderboard</h3>
+                    <a href="#" class="btn-download">
+                        <span class="text">See all</span>
+                        <i class='bx bx-right-arrow-alt'></i>
+                    </a>
+                </div>
+                <ul class="todo-list">
+                    <li class="juara1">
+                        <i class="las la-trophy"></i>
+                        <p>Kelas 7B</p>
+                        <a href="#" class="btn-download">
+                            <span class="text">500 point</span>
+                        </a>
+                    </li>
+                    <li class="juara2">
+                        <i class="las la-medal"></i>
+                        <p>Kelas 8B</p>
+                        <a href="#" class="btn-download">
+                            <span class="text">480 point</span>
+                        </a>
+                    </li>
+                    <li class="juara3">
+                        <i class="las la-award"></i>
+                        <p>Kelas 9A</p>
+                        <a href="#" class="btn-download">
+                            <span class="text">460 point</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
+@endsection
--- resources/views/siswa/layout.blade.php
+++ resources/views/siswa/layout.blade.php
@@ -24,65 +24,57 @@
     <title>CleanClass</title>
     <link rel="icon" href="assets/images/icon.ico">
 </head>
+<!-- SIDEBAR -->
+<section id="sidebar">
+    <a href="#" class="brand">
+        <img src="assets/images/started.png" alt="">
+        <span class="text">CleanClass</span>
+    </a>
+    <ul class="side-menu top">
+        <li class="active">
+            <a href="#">
+                <i class='bx bxs-dashboard'></i>
+                <span class="text">Home</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-trophy'></i>
+                <span class="text">Leaderboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-calendar'></i>
+                <span class="text">Jadwal</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-calendar-event'></i>
+                <span class="text">Event</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-file'></i>
+                <span class="text">Laporan</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="side-menu">
+        <li>
+            <a href="#" class="logout">
+                <i class='bx bxs-log-out-circle'></i>
+                <span class="text">Logout</span>
+            </a>
+        </li>
+    </ul>
+</section>
+<!-- SIDEBAR -->
+
 
 <body>
-    <div class="wrapper">
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="shadow"></div>
-        <div class="shadow"></div>
-        <div class="shadow"></div>
-    </div>
-
-    <!-- SIDEBAR -->
-    <section id="sidebar">
-        <a href="#" class="brand">
-            <img src="assets/images/started.png" alt="">
-            <span class="text">CleanClass</span>
-        </a>
-        <ul class="side-menu top">
-            <li class="active">
-                <a href="#">
-                    <i class='bx bxs-dashboard'></i>
-                    <span class="text">Home</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bx-trophy'></i>
-                    <span class="text">Leaderboard</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bxs-calendar'></i>
-                    <span class="text">Jadwal</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bxs-calendar-event'></i>
-                    <span class="text">Event</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bxs-file'></i>
-                    <span class="text">Laporan</span>
-                </a>
-            </li>
-        </ul>
-        <ul class="side-menu">
-            <li>
-                <a href="#" class="logout">
-                    <i class='bx bxs-log-out-circle'></i>
-                    <span class="text">Logout</span>
-                </a>
-            </li>
-        </ul>
-    </section>
-    <!-- SIDEBAR -->
 
     <!-- CONTENT -->
     <section id="content">
@@ -110,148 +102,9 @@
         <!-- NAVBAR -->
 
         <!-- MAIN -->
-        <main>
-            {{-- <div class="head-title">
-                <div class="left">
-                    <h1>Over<em>view</em></h1>
-                    </ul>
-                </div>
-            </div> --}}
-
-            <ul class="box-info">
-                <li>
-                    <i class='bx bxs-user'></i>
-                    <span class="text">
-                        <h3>Wali Kelas</h3>
-                        <p>Wildan Tisna Surya M.T.</p>
-                    </span>
-                </li>
-                {{-- <li>
-                    <i class='bx bxs-door-open'></i>
-                    <span class="text">
-                        <h3>Kelas</h3>
-                        <p>7B</p>
-                    </span>
-                </li> --}}
-                <li>
-                    <i class='bx bxs-medal'></i>
-                    <span class="text">
-                        <h3>Point</h3>
-                        <p>20</p>
-                    </span>
-                </li>
-                <li>
-                    <i class='bx bxs-calendar'></i>
-                    <span class="text">
-                        <h3>Berikutnya</h3>
-                        <p>Senin, 22 April 2024</p>
-                    </span>
-                </li>
-            </ul>
-
-
-
-            <div class="table-data">
-                <div class="order">
-                    <div class="head">
-                        <h3>Laporan</h3>
-                        <a href="#" class="btn-download">
-                            <span class="text">See all</span>
-                            <i class='bx bx-right-arrow-alt'></i>
-                        </a>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Waktu</th>
-                                <th>Status</th>
-                                <th>Keterangan</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <p>Senin, 4 Maret 2024</p>
-                                </td>
-                                <td>
-                                    <i class='bx bxs-check-circle'></i>
-                                </td>
-                                <td><span class="status completed">Piket</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p>Senin, 26 Februari 2024</p>
-                                </td>
-                                <td>
-                                    <i class='bx bxs-x-circle'></i>
-                                </td>
-                                <td><span class="status pending">Tidak Piket</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p>Senin, 19 Februari 2024</p>
-                                </td>
-                                <td>
-                                    <i class='bx bxs-check-circle'></i>
-                                </td>
-                                <td><span class="status completed">Piket</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p>Senin, 12 Februari 2024</p>
-                                </td>
-                                <td>
-                                    <i class='bx bxs-check-circle'></i>
-                                </td>
-                                <td><span class="status completed">Piket</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p>Senin, 5 Februari 2024</p>
-                                </td>
-                                <td>
-                                    <i class='bx bxs-check-circle'></i>
-                                </td>
-                                <td><span class="status completed">Piket</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="todo">
-                    <div class="head">
-                        <h3>Leaderboard</h3>
-                        <a href="#" class="btn-download">
-                            <span class="text">See all</span>
-                            <i class='bx bx-right-arrow-alt'></i>
-                        </a>
-                    </div>
-                    <ul class="todo-list">
-                        <li class="juara1">
-                            <i class="las la-trophy"></i>
-                            <p>Kelas 7B</p>
-                            <a href="#" class="btn-download">
-                                <span class="text">500 point</span>
-                            </a>
-                        </li>
-                        <li class="juara2">
-                            <i class="las la-medal"></i>
-                            <p>Kelas 8B</p>
-                            <a href="#" class="btn-download">
-                                <span class="text">480 point</span>
-                            </a>
-                        </li>
-                        <li class="juara3">
-                            <i class="las la-award"></i>
-                            <p>Kelas 9A</p>
-                            <a href="#" class="btn-download">
-                                <span class="text">460 point</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </main>
+        @yield('content')
         <!-- MAIN -->
+
     </section>
     <!-- CONTENT -->
     <script src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
